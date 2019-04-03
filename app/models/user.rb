0b1439fd30ad4c70e3bcaf3857d270e7433e16c8
JOhnny <jadonoso2@miuandes.cl>
2019-04-03 10:07:52 -0300

    algunas validaciones listas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :orders
+	validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+	validates :phone, length: {minimum: 9}
+	validates :phone, length: {maximum: 12}
 end
